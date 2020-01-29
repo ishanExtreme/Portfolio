64eb69e7d9a30fcf1934257284bd73e6934dc72f
ishanExtreme <ishan2198@hotmail.com>
2020-01-29 23:36:14 +0530

    project app ready

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
+import projects.views
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('',projects.views.homepage,name = 'homepage')
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Projects
 
 # Create your views here.
+def homepage(request):
+    projects = Projects.objects
+    return render(request,'projects/home.html',{"projects":projects})
