09e6e0e8f0800795288a033cb0ca533e2463bf28
ishanExtreme <ishan2198@hotmail.com>
2020-02-01 00:29:23 +0530

    portfolio v1 ready

--- blog/models.py
+++ blog/models.py
@@ -8,4 +8,9 @@ class Blogs(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='imageblog/')
 
-
+    def __str__(self):
+        return self.title
+    def summary(self):
+        return self.body[:100]
+    def pub_date_pretty(self):
+        return self.pub_date.strftime("%A %B %d")
