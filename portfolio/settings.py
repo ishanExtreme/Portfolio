ae6a864bb883315539c4e24a3aa0a571e351bcde
Ishan Mishra <ishan2198@hotmail.com>
2020-02-03 10:44:55 +0530

    travis_8

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,7 +76,17 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisci',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
+    }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
